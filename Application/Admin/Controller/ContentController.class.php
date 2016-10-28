8053cd421abf384ccb6b5c018e16d36f5132facc
d4smart <d4smart@foxmail.com>
2016-10-28 12:25:16 +0800

    完成缩略图上传功能

---
+++ Application/Admin/Controller/ContentController.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Desp:
+ * User: d4smart
+ * Date: 2016/10/28
+ * Time: 11:50
+ * Email:   d4smart@foxmail.com
+ * Github:  https://github.com/d4smart
+ */
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class ContentController extends CommonController
+{
+    public function index() {
+        $this->display();
+    }
+
+    public function add() {
+        $this->display();
+    }
+}
---
+++ Application/Admin/Controller/ImageController.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Desp:
+ * User: d4smart
+ * Date: 2016/10/28
+ * Time: 12:04
+ * Email:   d4smart@foxmail.com
+ * Github:  https://github.com/d4smart
+ */
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class ImageController extends CommonController
+{
+    private $_uploadObj;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function ajaxuploadimage() {
+        $upload = D("UploadImage");
+        $res = $upload->imageUpload();
+
+        if ($res === false) {
+            return show(0, "上传失败！", '');
+        } else {
+            return show(1, "上传成功！", $res);
+        }
+    }
+}
