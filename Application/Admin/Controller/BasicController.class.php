b73cd3a983f0efdfbedb0d38d472453386647634
d4smart <d4smart@foxmail.com>
2016-11-01 19:50:05 +0800

    网站信息配置功能

---
+++ Application/Admin/Controller/BasicController.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Desp:
+ * User: d4smart
+ * Date: 2016/11/1
+ * Time: 19:19
+ * Email:   d4smart@foxmail.com
+ * Github:  https://github.com/d4smart
+ */
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class BasicController extends CommonController
+{
+    public function index() {
+        $result = D("Basic")->select();
+        $this->assign('vo', $result);
+        $this->display();
+    }
+
+    public function add() {
+        if ($_POST) {
+            if (!I('title')) {
+                return show(0, "站点标题不能为空！");
+            }
+            if (!I('keywords')) {
+                return show(0, "站点关键词不能为空！");
+            }
+            if (!I('description')) {
+                return show(0, "站点描述不能为空！");
+            }
+
+            D("Basic")->save($_POST);
+            return show(1, "配置成功！");
+        } else {
+            return show(0, "没有提交的数据！");
+        }
+    }
+}
---
+++ Application/Common/Model/BasicModel.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Desp:
+ * User: d4smart
+ * Date: 2016/11/1
+ * Time: 19:26
+ * Email:   d4smart@foxmail.com
+ * Github:  https://github.com/d4smart
+ */
+
+namespace Common\Model;
+use Think\Model;
+
+class BasicModel extends Model
+{
+    public function __construct() {
+
+    }
+
+    public function save($data = array()) {
+        if (!$data) {
+            throw_exception("没有提交的数据！");
+        }
+        $id = F('basic_web_config', $data);
+        return $id;
+    }
+
+    public function select() {
+        return F('basic_web_config');
+    }
+}
